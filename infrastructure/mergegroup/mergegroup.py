ba4d06403db762571b77573474c4f6b750e131e9
Vincent <42785602+Vincent9339@users.noreply.github.com>
2025-03-06 21:22:56 +0100

    Update mergegroup.py

--- infrastructure/mergegroup/mergegroup.py
+++ infrastructure/mergegroup/mergegroup.py
@@ -7,7 +7,7 @@ if len(sys.argv) < 4:
 
 try:
     with open(sys.argv[-1], "r") as file:
-        whitelist = file.read()
+        whitelist = [line.strip() for line in file if not line.startswith("#")]
 except FileNotFoundError:
     print("Please provide a valid path to whitelist.txt")
     sys.exit(1)
@@ -15,27 +15,28 @@ except FileNotFoundError:
 output_dir = sys.argv[-2]
 os.makedirs(output_dir, exist_ok=True)
 
-input_dirs = sorted([f"{item}/{whitelist}" for item in sys.argv[1:-2]], 
-	     key=lambda x:[int(text) if text.isdigit() else text for text in re.split(r'(\d+)', x)])
+for i in range(len(whitelist)):
+	input_dirs = sorted([f"{item}/{whitelist[i]}" for item in sys.argv[1:-2]], 
+			 key=lambda x:[int(text) if text.isdigit() else text for text in re.split(r'(\d+)', x)])
 
-print(input_dirs)
+	print(input_dirs)
+	data_matrix = []
 
-data_matrix = []
-for input_dir in input_dirs:
-    if os.path.exists(input_dir):
-        try:
-            with open(input_dir, 'r') as f:
-                print(f"Merging: {input_dir} -> {output_dir}/group-merged.tsv")
-                reader = csv.reader(f, delimiter='\t')
-                row = [float(x) for x in next(reader)]
-                data_matrix.append(row)
-                
-        except (ValueError, StopIteration) as e:
-            print(e)
-            continue
-    else:
-        print(f"File not found: {input_dir}")
-        
+	for input_dir in input_dirs:
+		if os.path.exists(input_dir):
+		    try:
+		        with open(input_dir, 'r') as f:
+		            print(f"Merging: {input_dir} -> {output_dir}/group-merged.tsv")
+		            reader = csv.reader(f, delimiter='\t')
+		            row = [float(x) for x in next(reader)]
+		            data_matrix.append(row)
+		            
+		    except (ValueError, StopIteration) as e:
+		        print(e)
+		        continue
+		else:
+		    print(f"File not found: {input_dir}")
+		    
 
 if data_matrix:
     with open(output_dir+"/group-merged.tsv", "w") as file:
