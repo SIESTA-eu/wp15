332d1b391927b75623ced9faf8331c9ca99a42d5
Vincent <42785602+Vincent9339@users.noreply.github.com>
2025-04-04 14:36:44 +0200

    FIX for participant_nr as integer

--- infrastructure/singlesubject/singlesubject.py
+++ infrastructure/singlesubject/singlesubject.py
@@ -1,15 +1,18 @@
-import os, shutil, sys
+import os, shutil, sys, re
 import pandas as pd
 
 def singlesubject_(input_dir, output_dir ,participant_nr):
 
     try:
-        fl_dir = [dirs for root, dirs, _ in os.walk(input_dir)]
-        if participant_nr not in fl_dir[0]:
-            print(f"Error: participant_nr[{participant_nr}] does not exist in {input_dir}.")
-            pass   
-        else:         
-            output_base = os.path.join(os.getcwd(), f"{output_dir}_{participant_nr[4:]}")
+        p_id = sorted([item for item in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, item)) and item.startswith("sub-")])
+        try:
+            if participant_nr not in p_id and int(participant_nr):
+                participant_nr = p_id[int(participant_nr) -1]
+        except IndexError:
+            print("Error: participant_nr has exceeded the limit.")
+            sys.exit(1)
+        if participant_nr in p_id:
+            output_base = os.path.join(os.getcwd(), f"{output_dir}")
             for root, dirs, files in os.walk(input_dir):
                 rel_path = os.path.relpath(root, input_dir)
                 output_path = os.path.join(output_base, rel_path)
@@ -18,9 +21,13 @@ def singlesubject_(input_dir, output_dir ,participant_nr):
                     if file.endswith('.tsv'):
                         try:
                             df = pd.read_csv(os.path.join(root, file), sep='\t')
-                            if participant_nr in df['participant_id'].values:
-                                row = df[df['participant_id'] == participant_nr]
-                                row.to_csv(os.path.join(output_path, file), sep='\t', index=False)
+                            try:
+        	                    if participant_nr in df[list(df.columns)[0]].values:
+        	                        row = df[df[list(df.columns)[0]] == participant_nr]
+        	                        row.to_csv(os.path.join(output_path, file), sep='\t', index=False)
+                            except Exception as e:
+                                print(e)
+                                pass
                         except:
                             print("Error: participant_nr {participant_nr} not found in the file: {os.path.join(root, file)}")
                     if not file.endswith('.tsv'):
@@ -33,7 +40,7 @@ def singlesubject_(input_dir, output_dir ,participant_nr):
                     #        shutil.copy2(os.path.join(root, file), output_path)
                     #    except FileExistsError:
                     #        pass
-		        
+        	    
                 for dir in dirs:
                     if dir == participant_nr:
                         src_dir = os.path.join(root, dir)
@@ -42,7 +49,7 @@ def singlesubject_(input_dir, output_dir ,participant_nr):
                             shutil.copytree(src_dir, dst_dir)
                         except FileExistsError:
                             pass 
-		        
+        	    
                 for root, dirs, files in os.walk(output_base, topdown=False):
                     for dir in dirs:
                         dir_path = os.path.join(root, dir)
@@ -50,9 +57,11 @@ def singlesubject_(input_dir, output_dir ,participant_nr):
                             try:
                                 shutil.rmtree(dir_path)
                             except OSError as e:
-                                print("Error: to investigate")
+                                print("Error: {e}")
                                 pass
-            print(f"{participant_nr} has been successfully processed.")        
+            print(f"{participant_nr} has been succesfully splitted to {str(output_dir)}")        
+        else:
+            print(f"Error: participant_nr[{participant_nr}] does not exist in {input_dir}.")  
         return True
     except FileNotFoundError:
         print("Error: Input folder Not Found. Please make sure of input folder name.", file=sys.stderr)
@@ -66,6 +75,7 @@ def main():
     output_dir = sys.argv[2]
     participant_nr = sys.argv[3]
     pid = singlesubject_(input_dir, output_dir,participant_nr)
+    
     if not pid:
         print("Nothing to return!")
         sys.exit(1)
