a0ec5e9a16ff8a4fe6e1ab6855e97bd73e7c1c83
Robert Oostenveld <r.oostenveld@gmail.com>
2025-04-23 17:30:03 +0200

    skip directories that should not be copied anyway

--- infrastructure/singlesubject/singlesubject.py
+++ infrastructure/singlesubject/singlesubject.py
@@ -1,19 +1,26 @@
 import os, shutil, sys, re
 import pandas as pd
 
-def singlesubject_(input_dir, output_dir ,participant_nr):
+def singlesubject_(input_dir, output_dir, participant_id):
 
     try:
-        p_id = sorted([item for item in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, item)) and item.startswith("sub-")])
-        try:
-            if participant_nr not in p_id and int(participant_nr):
-                participant_nr = p_id[int(participant_nr) -1]
-        except IndexError:
-            print("Error: participant_nr has exceeded the limit.")
-            sys.exit(1)
-        if participant_nr in p_id:
+        participant_ids = sorted([item for item in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, item)) and item.startswith("sub-")])
+        if participant_id in participant_ids:
+            # the participant_id was specified as a string
+            pass
+        elif participant_id not in participant_ids and int(participant_id):
+            # the participant_id was specified as a positive number
+            participant_id = participant_ids[int(participant_id) - 1]
+
+        if participant_id in participant_ids:
+            skip_ids = [id for id in participant_ids if id != participant_id]
+
             output_base = os.path.join(os.getcwd(), f"{output_dir}")
             for root, dirs, files in os.walk(input_dir):
+                if len(set(root.split(os.sep)) & set(skip_ids)) > 0:
+                    # skip any directories that contain another participant ID
+                    continue
+
                 rel_path = os.path.relpath(root, input_dir)
                 output_path = os.path.join(output_base, rel_path)
                 os.makedirs(output_path, exist_ok=True)
@@ -22,14 +29,14 @@ def singlesubject_(input_dir, output_dir ,participant_nr):
                         try:
                             df = pd.read_csv(os.path.join(root, file), sep='\t')
                             try:
-                                if participant_nr in df[list(df.columns)[0]].values:
-                                    row = df[df[list(df.columns)[0]] == participant_nr]
+                                if participant_id in df[list(df.columns)[0]].values:
+                                    row = df[df[list(df.columns)[0]] == participant_id]
                                     row.to_csv(os.path.join(output_path, file), sep='\t', index=False)
                             except Exception as e:
                                 print(e)
                                 pass
                         except:
-                            print("Error: participant_nr {participant_nr} not found in the file: {os.path.join(root, file)}")
+                            print("Error: {participant_id} not found in the file: {os.path.join(root, file)}")
                     if not file.endswith('.tsv'):
                         try:
                             shutil.copy2(os.path.join(root, file), output_path)
@@ -37,7 +44,7 @@ def singlesubject_(input_dir, output_dir ,participant_nr):
                             pass
 
                 for dir in dirs:
-                    if dir == participant_nr:
+                    if dir == participant_id:
                         src_dir = os.path.join(root, dir)
                         dst_dir = os.path.join(output_path, dir)
                         try:
@@ -48,15 +55,15 @@ def singlesubject_(input_dir, output_dir ,participant_nr):
                 for root, dirs, files in os.walk(output_base, topdown=False):
                     for dir in dirs:
                         dir_path = os.path.join(root, dir)
-                        if str(participant_nr[:4]) in dir_path and str(participant_nr) not in dir_path:
+                        if str(participant_id[:4]) in dir_path and str(participant_id) not in dir_path:
                             try:
                                 shutil.rmtree(dir_path)
                             except OSError as e:
                                 print("Error: {e}")
                                 pass
-            print(f"{participant_nr} has been successfully split to {str(output_dir)}")        
+            print(f"{participant_id} has been successfully split to {str(output_dir)}")        
         else:
-            print(f"Error: participant_nr[{participant_nr}] does not exist in {input_dir}.")  
+            print(f"Error: the participant {participant_id} does not exist in {input_dir}.")  
         return True
     except FileNotFoundError:
         print("Error: Input folder Not Found. Please make sure of input folder name.", file=sys.stderr)
@@ -70,8 +77,8 @@ def main(args=None):
         sys.exit(1)
     input_dir = args[1]
     output_dir = args[2]
-    participant_nr = args[3]
-    pid = singlesubject_(input_dir, output_dir,participant_nr)
+    participant_id = args[3] # this can be either a positive number or the participant ID itself
+    pid = singlesubject_(input_dir, output_dir, participant_id)
     
     if not pid:
         print("Nothing to return!")
