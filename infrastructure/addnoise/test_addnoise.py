53e47c067dfe32e136a0caaf32a6aa3de42a3b1d
Robert Oostenveld <r.oostenveld@gmail.com>
2025-06-26 19:13:13 +0200

    convert into proper pytest

--- infrastructure/addnoise/test_addnoise.py
+++ infrastructure/addnoise/test_addnoise.py
@@ -1,12 +1,40 @@
+#!/usr/bin/env python3
+
 import os
 import pytest
 import tempfile
 from pathlib import Path
-from addnoise import addnoise
+import addnoise
 
-test_path = Path('tests')
-test_dirs = [d for d in test_path.iterdir() if d.is_dir() and d.name.startswith('test')]
 
-for input_dir in test_dirs:
-    output_dir = tempfile.TemporaryDirectory(delete=True)
-    addnoise(input_dir, output_dir.name, os.path.join(input_dir, 'whitelist.txt'), os.path.join(input_dir, 'noise.tsv'))
+def test_addnoise():
+    """
+    This test adds noise to files in a given directory using the addnoise module.
+    It checks if the output directory is created and if the noise file exists.
+    """
+
+    test_path = Path('tests')
+    test_dirs = [d for d in test_path.iterdir() if d.is_dir() and d.name.startswith('test')]
+    for input_dir in test_dirs:
+        print('-' * 80)
+        print(f"Testing addnoise on directory: {input_dir}")
+        output_dir = tempfile.TemporaryDirectory(delete=True)
+
+        args = [
+            'exe',
+            input_dir, 
+            output_dir.name, 
+            os.path.join(input_dir, 'whitelist.txt'), 
+            os.path.join(input_dir, 'noise.tsv')
+        ]
+
+        addnoise.main(args)
+        assert os.path.exists(output_dir.name), f"Output directory {output_dir.name} was not created"
+        
+        # this still fails, because writing mat files is not implemented yet
+        # assert os.listdir(output_dir.name), f"Output directory {output_dir.name} is empty"
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, '-v', '-s'])
+
