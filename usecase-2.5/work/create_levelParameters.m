2f45d634f23cd65150999bb15890713b02d5f523
Marque-CerCo <adrien.romain.marque@gmail.com>
2025-07-16 14:31:24 +0200

    Update create_levelParameters.m

--- usecase-2.5/work/create_levelParameters.m
+++ usecase-2.5/work/create_levelParameters.m
@@ -1,7 +1,7 @@
-function struct_level = create_levelParameters(TR, name, events, onset, duration)
+function struct_level = create_levelParameters(TR, events, onset, duration)
     struct_level.timing_units = 'secs';
     struct_level.timing_RT = TR;
-    struct_level.conditionName = events;
+    struct_level.conditionEvent = events;
     struct_level.conditionOnset = onset;
     struct_level.conditionDuration = duration;
 end
